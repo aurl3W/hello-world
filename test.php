a13aeaab0c5f6a920c16c31055b99eb4baff816e
aurl3W <41710448+aurl3W@users.noreply.github.com>
2018-07-26 03:06:35 +0200

    Create test.php

---
+++ test.php
@@ -0,0 +1,98 @@
+<!DOCTYPE html>
+<html lang="fr">
+<head>
+<meta charset="utf-8" />
+<meta name="robots" content="index, follow, all" />
+<meta name="Keywords" content="" />
+<meta name="author" content="Aur&eacute;lien Labrange - Aur&eacute;L3w" />
+<meta name="Description" content="3wRadio" />
+<meta name=viewport content="width=device-width, initial-scale=1">
+<title>CV</title>
+<link rel="stylesheet" type="text/css" href="css/class_global.css" />
+
+</head>
+
+<body>
+<!--
+	<div class="barre_btns_menu">
+
+			<ul>
+				<li>
+					<a href="" class="boutons_menu">toto</a>
+				</li>
+				<li>
+					<a href="" class="boutons_menu">toto</a>
+				</li>
+				<li>
+					<a href="" class="boutons_menu">toto</a>
+				</li>
+			</ul>
+	
+	</div>
+-->	
+
+	<!--div class="barre_btns_menu">
+	<span class="etiquette_menu">Menu</span>
+		<div id="container">
+			<nav id="menu">
+					<ul>
+						<li>
+							<a href="?" onclick="load_page(this.href);return false;" class="boutons_menu" title="Présentation">Présentation</a>
+						</li>
+					
+						<li>
+							<a href="?" onclick="load_page(this.href);return false;" class="boutons_menu" title="Compétences">Compétences</a>
+						</li>
+						
+						<li>
+							<a href="?" onclick="load_page(this.href);return false;" class="boutons_menu" title="Formations">Formations</a>
+						</li>
+						
+						<li>
+							<a href="?" onclick="load_page(this.href);return false;" class="boutons_menu" title="Expériences professionnelles">Expériences professionnelles</a>
+						</li>
+					
+						<li>
+							<a href="?" onclick="load_contact(this.href);return false;" class="boutons_menu" title="Contact">Contact</a>
+						</li>
+					</ul>
+			</nav>
+		</div>
+	</div-->
+	
+	
+	<div style="z-index: 500; opacity: 0.5; position:fixed; left:0px; top:0px; border: none; background: #aaa; padding: 16px; width: 100%; height: auto; color: #fff; clear: both;">
+	
+		main menu
+	
+	</div>
+	
+	<div style="position:relative; top:50px;">
+	
+	
+	<div style="position:relative; left:-8px; top:-8px; border: none; background: steelblue; padding-top: 16px; padding-bottom: 16px; padding-right: 16px; width: 100%; height: auto; color: #fff; clear: both;">
+	
+		<div style="margin-left: 16px; font-weight: bold;">Qui suis-je...?</div>
+	
+	</div>
+	<div style="position:relative; left:-8px; top:-8px; border: none; border-bottom: solid 3px #aaa; background: #eee; padding-top: 10px; padding-bottom: 10px; padding-right: 16px; width: 100%; height: auto; color: #000; text-align: justify;">
+	
+		<div style="margin-left: 10px; margin-right:-4px;">
+			<p>Passionné d'informatique, du web et de tout ce qui est électronique depuis toujours et de nature auto-didact, je propose désormais mes services dans le monde de la création de sites internet.</p>
+			<p>
+				En 2007, lors d'une formation en Maintenance des Systèmes et Réseaux Informatique, j'ai vu et appris les bases du langage du web, suite a ces cours, j'ai toute suite été intérressé et passioné par les résultats, les outils utilisés et la magie que prennait avec l'utilisation des différents langages pour réaliser de simple page.<br />
+				A l'issue de cette formation, j'ai continuer dans l'édition et la programmation web en-tant qu'auto-didact.<br />
+			</p>
+			<p>
+				Par la suite, j'ai suivi une autre formation en infographiste - metteur en page - webdesign, ou j'y ai suivi des cours en web un peut plus élargis. Ce qui fait, que lors de cette expérience, j'y ai enrichie mes connaissances et compétences en y apprenant l'utilisation de nouveaux outils et de nouvelles methodes, afin de réaliser des pages web un peu plus intérréssente.<br />
+				Encore une fois, a l'issue de cette fornation, j'ai continuer en-tant qu'auto-didact et lors de mes expériences, j'ai vu et assimillé des nouvelles technique, afin, de réaliser et développer des sites et pages plus dynamique et attrayante.
+				<br />Au fur et a mesure, j'ai adaptés mes propres méthodes de travail et techniques, afin d'obtenir et de faire en sorte de développer des pages plus riche dans un style classique, simple d'utilisation et moderne a la fois, tout en m'inspirant des styles et techniques déjà existentes.
+			</p>
+		</div>
+	
+	</div>
+	
+	
+	</div>
+	
+</body></html>
